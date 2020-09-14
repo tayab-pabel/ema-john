86b60ab98403570ce5c26c90fb250211d33f6895
tayab-pabel <tayabpabel@yahoo.com>
2020-09-14 20:54:08 +0600

    v1

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header/Header';
+import Shop from './components/Shop/Shop';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Review from './components/Review/Review';
+import Manage from './components/Manage/Manage';
+import NotFound from './components/NotFound/NotFound';
+import ProductDetail from './components/ProductDetail/ProductDetail';
+
+function App() {
+  return (
+    <Router>
+      <Header></Header>
+      <Switch>
+        <Route path="/shop">
+          <Shop></Shop>
+        </Route>
+        <Route path="/review">
+          <Review></Review>
+        </Route>
+        <Route path="/manage">
+          <Manage></Manage>
+        </Route>
+        <Route exact path="/">
+          <Shop></Shop>
+        </Route>
+        <Route path="/product/:productKey">
+          <ProductDetail></ProductDetail>
+        </Route>
+        <Route exact path="*">
+          <NotFound></NotFound>
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
